73c8c11bd8c9b17c4bc500f18716f897d496a046
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-11-15 23:17:50 +0900

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function on_text_change(){
+	ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  ctx.save();
+  
+	let text_content = document.getElementById("text1").value;
+  console.log(text_content);
+  
+  ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = "300px serif";
+	ctx.fillStyle = "lime";
+	
+	ctx.scale(1/text_content.length,1);
+	ctx.fillText(text_content, 0, 260);
+  ctx.restore();
+}
+
+function on_save_click(){
+	let a = document.createElement('a');
+	a.href = canvas.toDataURL();
+  a.download = "result.png";
+  a.click();
+}
